8fc31d5b51a3aba5a06f2bf5bf9e4dcb77dce44e
“Ysachid48 <yosachid48@gmail.com>
2023-02-23 12:03:25 +0000

    Google Store

---
+++ src/components/AppItem/index.js
@@ -0,0 +1,19 @@
+import './index.css'
+
+const AppItem = props => {
+  const {appDetails} = props
+  const {appName, imageUrl} = appDetails
+
+  return (
+    <li className="app-item">
+      <img className="app-image" src={imageUrl} alt={appName} />
+      <p className="app-name">{appName}</p>
+      <button type="button" className="download">
+        {' '}
+        Download
+      </button>
+    </li>
+  )
+}
+
+export default AppItem
